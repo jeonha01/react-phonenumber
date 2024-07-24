a4d03b07f8ec0b99357f48c93473a55701b16b6f
Jeonha Kang <kangjeonha@gmail.com>
2024-07-24 19:56:00 +0900

    third-commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import { createStore } from "redux";
+import { configureStore } from "redux";
 import reducer from "./reducer/reducer";
 
-let store = createStore(reducer)
+let store = configureStore(reducer)
 
 export default store
